bf1f054b865e854832eac5f6f03a0bd7808da3b4
Sripriya Sundararaman <spreiyah@yahoo.com>
2021-06-08 19:49:28 +1200

    Add project files.

---
+++ Comparetwonos/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Comparetwonos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            Console.WriteLine("Enter a:");
+            a=Convert.ToInt32(Console.ReadLine());//This statement is to convert string to integer
+            Console.WriteLine("Enter b:");
+            b = Convert.ToInt32(Console.ReadLine());
+            
+            if (a > b)
+            {
+                Console.WriteLine(a.ToString() + " is greater");
+            }
+            else
+            {
+                Console.WriteLine(b.ToString() + " is greater");
+            }
+           
+        }
+    }
+}
